9e3c7b45a0325b0b067b07603929004728630e9b
alumno <alumno@LAB7-3>
2017-05-19 09:23:15 -0600

    Agregar archivos de proyecto.

---
+++ comunidad/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace comunidad.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ comunidad/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comunidad.Startup))]
+namespace comunidad
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
